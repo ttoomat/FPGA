af25155f9c6ab5e3a8e6550a2ccd83f4dd6ec009
Troitskaya Tamara <troitskayatamara@mail.ru>
2025-10-22 18:29:14 +0300

    Button + LED.

--- lab4/lab4/src/top.v
+++
@@ -1,6 +0,0 @@
-module top(
-    input Power,
-    output LED4
-);
-assign LED4 = Power;
-endmodule
---
+++ lab4/src/top.v
@@ -0,0 +1,7 @@
+// если нажата кнопка, то горит светодиод 4
+module top(
+    input A,
+    output LED4
+);
+assign LED4 = A;
+endmodule
